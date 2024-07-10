802719361de03ea4fd846123389b65b31c639681
Saru-12 <Sarutiwaskar@gmail.com>
2024-07-10 15:46:50 +0530

    Initial commit

---
+++ my-strapi-project/src/api/newproject/controllers/newproject.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newproject controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::newproject.newproject');
---
+++ my-strapi-project/src/api/newproject/routes/newproject.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newproject router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::newproject.newproject');
